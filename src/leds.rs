d047663534d59e094fb928e1a3eedffccc448aea
Paul Daniel Faria <Nashenas88@users.noreply.github.com>
2022-01-25 13:56:33 -0800

    Convert to rtic, add support for pico so debugging is possible

--- src/leds.rs
+++ src/leds.rs
@@ -1,33 +1,15 @@
-use adafruit_kb2040::hal::gpio::{Function, FunctionConfig, PinId, ValidPinMode};
-use adafruit_kb2040::hal::pio::{PIOExt, StateMachineIndex};
+use core::fmt::Debug;
 use core::iter::once;
-use embedded_hal::timer::CountDown;
-use embedded_time::duration::Microseconds;
 use keyberon::keyboard::Leds;
 use smart_leds::{brightness, SmartLedsWrite, RGB8};
-use ws2812_pio::Ws2812;
 
-pub(crate) struct KbLeds<P, SM, C, I>
-where
-    I: PinId,
-    C: CountDown,
-    P: PIOExt + FunctionConfig,
-    Function<P>: ValidPinMode<I>,
-    SM: StateMachineIndex,
-{
-    pub(crate) ws: Ws2812<P, SM, C, I>,
+pub(crate) struct KbLeds<WS> {
+    pub(crate) ws: WS,
     color: RGB8,
 }
 
-impl<P, SM, C, I> KbLeds<P, SM, C, I>
-where
-    I: PinId,
-    C: CountDown,
-    P: PIOExt + FunctionConfig,
-    Function<P>: ValidPinMode<I>,
-    SM: StateMachineIndex,
-{
-    pub(crate) fn new(ws: Ws2812<P, SM, C, I>) -> Self {
+impl<WS> KbLeds<WS> {
+    pub(crate) fn new(ws: WS) -> Self {
         Self {
             ws,
             color: RGB8 { r: 0, g: 0, b: 0 },
@@ -35,14 +17,11 @@ where
     }
 }
 
-impl<P, SM, C, I> Leds for KbLeds<P, SM, C, I>
+impl<WS> Leds for KbLeds<WS>
 where
-    I: PinId,
-    C: CountDown,
-    P: PIOExt + FunctionConfig,
-    Function<P>: ValidPinMode<I>,
-    SM: StateMachineIndex,
-    <C as CountDown>::Time: From<Microseconds>,
+    WS: SmartLedsWrite,
+    <WS as SmartLedsWrite>::Error: Debug,
+    <WS as SmartLedsWrite>::Color: From<RGB8>,
 {
     fn num_lock(&mut self, status: bool) {
         self.color.r = if status { 255 } else { 0 };
--- src/main.rs
+++ src/main.rs
@@ -8,92 +8,147 @@
 #![no_std]
 #![no_main]
 
-use cortex_m_rt::entry;
-use embedded_hal::timer::CountDown;
-use embedded_time::duration::Extensions;
 use panic_halt as _;
 
-use adafruit_kb2040::hal::clocks::{init_clocks_and_plls, Clock};
-use adafruit_kb2040::hal::pio::PIOExt;
-use adafruit_kb2040::hal::timer::Timer;
-use adafruit_kb2040::hal::usb::UsbBus;
-use adafruit_kb2040::hal::watchdog::Watchdog;
-use adafruit_kb2040::hal::{pac, Sio};
-use adafruit_kb2040::XOSC_CRYSTAL_FREQ;
+#[cfg(feature = "kb2040")]
+use adafruit_kb2040 as bsp;
+#[cfg(feature = "rp-pico")]
+use rp_pico as bsp;
+
+#[cfg(feature = "kb2040")]
+use bsp::hal::clocks::Clock;
+use bsp::hal::clocks::{init_clocks_and_plls, PeripheralClock};
+#[cfg(feature = "kb2040")]
+use bsp::hal::pio::PIOExt;
+use bsp::hal::timer::Timer;
+use bsp::hal::usb::UsbBus;
+use bsp::hal::watchdog::Watchdog;
+use bsp::hal::{pac, Sio};
+use bsp::pac::{PIO0, RESETS};
+use bsp::XOSC_CRYSTAL_FREQ;
+#[cfg(feature = "kb2040")]
+use core::fmt::Debug;
+#[cfg(feature = "kb2040")]
+use smart_leds::SmartLedsWrite;
+#[cfg(feature = "kb2040")]
+use smart_leds::RGB8;
 use usb_device::class_prelude::UsbBusAllocator;
+#[cfg(feature = "kb2040")]
 use ws2812_pio::Ws2812;
 
+#[cfg(feature = "kb2040")]
 mod leds;
 
-static mut USB_BUS: Option<UsbBusAllocator<adafruit_kb2040::hal::usb::UsbBus>> = None;
+static mut USB_BUS: Option<UsbBusAllocator<bsp::hal::usb::UsbBus>> = None;
 
-/// Entry point to our bare-metal application.
-///
-/// The `#[entry]` macro ensures the Cortex-M start-up code calls this
-/// function as soon as all global variables are initialised.
-///
-/// The function configures the RP2040 peripherals, then the LED, then runs
-/// the colour wheel in an infinite loop.
-#[entry]
-fn main() -> ! {
-    // Configure the RP2040 peripherals
+#[rtic::app(device = bsp::pac, peripherals = true, dispatchers = [PIO0_IRQ_0])]
+mod app {
+    use super::*;
 
-    let mut pac = pac::Peripherals::take().unwrap();
-    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    static mut USB_BUS: Option<usb_device::bus::UsbBusAllocator<bsp::hal::usb::UsbBus>> = None;
 
-    let clocks = init_clocks_and_plls(
-        XOSC_CRYSTAL_FREQ,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .ok()
-    .unwrap();
+    #[shared]
+    struct Shared {}
 
-    let sio = Sio::new(pac.SIO);
+    #[local]
+    struct Local {}
 
-    let pins = adafruit_kb2040::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
+    /// Entry point to our bare-metal application.
+    ///
+    /// The `#[entry]` macro ensures the Cortex-M start-up code calls this
+    /// function as soon as all global variables are initialised.
+    ///
+    /// The function configures the RP2040 peripherals, then the LED, then runs
+    /// the colour wheel in an infinite loop.
+    #[init]
+    fn init(c: init::Context) -> (Shared, Local, init::Monotonics) {
+        // Configure the RP2040 peripherals
 
-    let timer = Timer::new(pac.TIMER, &mut pac.RESETS);
-    let mut delay = timer.count_down();
+        let mut pac = pac::Peripherals::take().unwrap();
+        let mut watchdog = Watchdog::new(pac.WATCHDOG);
 
+        let clocks = init_clocks_and_plls(
+            XOSC_CRYSTAL_FREQ,
+            pac.XOSC,
+            pac.CLOCKS,
+            pac.PLL_SYS,
+            pac.PLL_USB,
+            &mut pac.RESETS,
+            &mut watchdog,
+        )
+        .ok()
+        .unwrap();
+
+        let sio = Sio::new(pac.SIO);
+
+        let pins = bsp::Pins::new(
+            pac.IO_BANK0,
+            pac.PADS_BANK0,
+            sio.gpio_bank0,
+            &mut pac.RESETS,
+        );
+
+        let timer = Timer::new(pac.TIMER, &mut pac.RESETS);
+        let mut _delay = timer.count_down();
+
+        let usb_bus = UsbBusAllocator::new(UsbBus::new(
+            pac.USBCTRL_REGS,
+            pac.USBCTRL_DPRAM,
+            clocks.usb_clock,
+            true,
+            &mut pac.RESETS,
+        ));
+
+        let kb_leds = get_leds(
+            pac.PIO0,
+            &mut pac.RESETS,
+            pins,
+            &clocks.peripheral_clock,
+            &timer,
+        );
+        unsafe {
+            USB_BUS = Some(usb_bus);
+        }
+
+        let _usb_class = keyberon::new_class(unsafe { USB_BUS.as_ref().unwrap() }, kb_leds);
+        let _usb_dev = keyberon::new_device(unsafe { USB_BUS.as_ref().unwrap() });
+
+        (Shared {}, Local {}, init::Monotonics())
+
+        // loop {
+        //     delay.start(25.milliseconds());
+        //     let _ = nb::block!(delay.wait());
+        // }
+    }
+}
+
+#[cfg(feature = "kb2040")]
+fn get_leds<'a>(
+    pio0: PIO0,
+    resets: &'a mut RESETS,
+    pins: bsp::Pins,
+    peripheral_clock: &'a PeripheralClock,
+    timer: &'a Timer,
+) -> leds::KbLeds<impl SmartLedsWrite<Color = impl From<RGB8>, Error = impl Debug> + 'a> {
     // Configure the addressable LED
-    let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
-
-    let usb_bus = UsbBusAllocator::new(UsbBus::new(
-        pac.USBCTRL_REGS,
-        pac.USBCTRL_DPRAM,
-        clocks.usb_clock,
-        true,
-        &mut pac.RESETS,
-    ));
-
+    let (mut pio, sm0, _, _, _) = pio0.split(resets);
     let ws = Ws2812::new(
         pins.neopixel.into_mode(),
         &mut pio,
         sm0,
-        clocks.peripheral_clock.freq(),
+        peripheral_clock.freq(),
         timer.count_down(),
     );
 
-    let kb_leds = leds::KbLeds::new(ws);
-    unsafe {
-        USB_BUS = Some(usb_bus);
-    }
+    leds::KbLeds::new(ws)
+}
 
-    let _usb_class = keyberon::new_class(unsafe { USB_BUS.as_ref().unwrap() }, kb_leds);
-    let _usb_dev = keyberon::new_device(unsafe { USB_BUS.as_ref().unwrap() });
-
-    loop {
-        delay.start(25.milliseconds());
-        let _ = nb::block!(delay.wait());
-    }
+#[cfg(feature = "pico")]
+fn get_leds(
+    _pac: PIO0,
+    _resets: &mut RESETS,
+    _pins: bsp::Pins,
+    _peripheral_clock: &PeripheralClock,
+    _timer: &Timer,
+) {
 }
