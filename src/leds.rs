3ef547e8baad600a344857d60114b9ac02eb1caa
Paul Daniel Faria <Nashenas88@users.noreply.github.com>
2022-01-24 22:43:55 -0800

    Add led support for keys

---
+++ src/leds.rs
@@ -0,0 +1,66 @@
+use adafruit_kb2040::hal::gpio::{Function, FunctionConfig, PinId, ValidPinMode};
+use adafruit_kb2040::hal::pio::{PIOExt, StateMachineIndex};
+use core::iter::once;
+use embedded_hal::timer::CountDown;
+use embedded_time::duration::Microseconds;
+use keyberon::keyboard::Leds;
+use smart_leds::{brightness, SmartLedsWrite, RGB8};
+use ws2812_pio::Ws2812;
+
+pub(crate) struct KbLeds<P, SM, C, I>
+where
+    I: PinId,
+    C: CountDown,
+    P: PIOExt + FunctionConfig,
+    Function<P>: ValidPinMode<I>,
+    SM: StateMachineIndex,
+{
+    pub(crate) ws: Ws2812<P, SM, C, I>,
+    color: RGB8,
+}
+
+impl<P, SM, C, I> KbLeds<P, SM, C, I>
+where
+    I: PinId,
+    C: CountDown,
+    P: PIOExt + FunctionConfig,
+    Function<P>: ValidPinMode<I>,
+    SM: StateMachineIndex,
+{
+    pub(crate) fn new(ws: Ws2812<P, SM, C, I>) -> Self {
+        Self {
+            ws,
+            color: RGB8 { r: 0, g: 0, b: 0 },
+        }
+    }
+}
+
+impl<P, SM, C, I> Leds for KbLeds<P, SM, C, I>
+where
+    I: PinId,
+    C: CountDown,
+    P: PIOExt + FunctionConfig,
+    Function<P>: ValidPinMode<I>,
+    SM: StateMachineIndex,
+    <C as CountDown>::Time: From<Microseconds>,
+{
+    fn num_lock(&mut self, status: bool) {
+        self.color.r = if status { 255 } else { 0 };
+        self.ws.write(brightness(once(self.color), 32)).unwrap();
+    }
+
+    fn caps_lock(&mut self, status: bool) {
+        self.color.g = if status { 255 } else { 0 };
+        self.ws.write(brightness(once(self.color), 32)).unwrap();
+
+    }
+
+    fn scroll_lock(&mut self, status: bool) {
+        self.color.b = if status { 255 } else { 0 };
+        self.ws.write(brightness(once(self.color), 32)).unwrap();
+    }
+
+    fn compose(&mut self, _status: bool) {}
+
+    fn kana(&mut self, _status: bool) {}
+}
--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,24 @@
 #![no_std]
 #![no_main]
 
-use core::iter::once;
 use cortex_m_rt::entry;
 use embedded_hal::timer::CountDown;
 use embedded_time::duration::Extensions;
 use panic_halt as _;
 
-use adafruit_kb2040::{
-    hal::{
-        clocks::{init_clocks_and_plls, Clock},
-        pac,
-        pio::PIOExt,
-        timer::Timer,
-        watchdog::Watchdog,
-        Sio,
-    },
-    XOSC_CRYSTAL_FREQ,
-};
-use smart_leds::{brightness, SmartLedsWrite, RGB8};
+use adafruit_kb2040::hal::clocks::{init_clocks_and_plls, Clock};
+use adafruit_kb2040::hal::pio::PIOExt;
+use adafruit_kb2040::hal::timer::Timer;
+use adafruit_kb2040::hal::usb::UsbBus;
+use adafruit_kb2040::hal::watchdog::Watchdog;
+use adafruit_kb2040::hal::{pac, Sio};
+use adafruit_kb2040::XOSC_CRYSTAL_FREQ;
+use usb_device::class_prelude::UsbBusAllocator;
 use ws2812_pio::Ws2812;
+
+mod leds;
+
+static mut USB_BUS: Option<UsbBusAllocator<adafruit_kb2040::hal::usb::UsbBus>> = None;
 
 /// Entry point to our bare-metal application.
 ///
@@ -69,7 +68,15 @@ fn main() -> ! {
     // Configure the addressable LED
     let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
 
-    let mut ws = Ws2812::new(
+    let usb_bus = UsbBusAllocator::new(UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+
+    let ws = Ws2812::new(
         pins.neopixel.into_mode(),
         &mut pio,
         sm0,
@@ -77,33 +84,16 @@ fn main() -> ! {
         timer.count_down(),
     );
 
-    // Infinite colour wheel loop
+    let kb_leds = leds::KbLeds::new(ws);
+    unsafe {
+        USB_BUS = Some(usb_bus);
+    }
 
-    let mut n: u8 = 128;
+    let _usb_class = keyberon::new_class(unsafe { USB_BUS.as_ref().unwrap() }, kb_leds);
+    let _usb_dev = keyberon::new_device(unsafe { USB_BUS.as_ref().unwrap() });
+
     loop {
-        ws.write(brightness(once(wheel(n)), 32)).unwrap();
-        n = n.wrapping_add(1);
-
         delay.start(25.milliseconds());
         let _ = nb::block!(delay.wait());
-    }
-}
-
-/// Convert a number from `0..=255` to an RGB color triplet.
-///
-/// The colours are a transition from red, to green, to blue and back to red.
-fn wheel(mut wheel_pos: u8) -> RGB8 {
-    wheel_pos = 255 - wheel_pos;
-    if wheel_pos < 85 {
-        // No green in this sector - red and blue only
-        (255 - (wheel_pos * 3), 0, wheel_pos * 3).into()
-    } else if wheel_pos < 170 {
-        // No red in this sector - green and blue only
-        wheel_pos -= 85;
-        (0, wheel_pos * 3, 255 - (wheel_pos * 3)).into()
-    } else {
-        // No blue in this sector - red and green only
-        wheel_pos -= 170;
-        (wheel_pos * 3, 255 - (wheel_pos * 3), 0).into()
     }
 }
