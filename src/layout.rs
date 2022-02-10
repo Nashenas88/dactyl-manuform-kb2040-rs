b3d3abb7e64bb0f9fde6ab3073e980ca8a7f0232
Paul Daniel Faria <Nashenas88@users.noreply.github.com>
2022-02-09 19:57:06 -0800

    Update layout to be more comfortable

--- src/layout.rs
+++ src/layout.rs
@@ -1,30 +1,28 @@
 use keyberon::action;
 use keyberon::action::Action;
-use keyberon::key_code::KeyCode::*;
 
-const AL_T: Action = action::m(&[LAlt, Tab]);
 const COLEMAK: Action = action::d(0);
 const QWERTY: Action = action::d(1);
-const NUM_PAD: Action = action::d(2);
+const NUM_PAD: Action = action::l(2);
 
 pub static LAYERS: keyberon::layout::Layers = keyberon::layout::layout! {
     { // (0) Colemak Dh
-        [F1     F2 F3   F4     F5        F6        F7       F8     F9  F10 F11 F12   ]
-        ['`'    1  2    3      4         5         6        7      8   9   0   =     ]
-        [Tab    Q  W    F      P         B         J        L      U   Y   ;   -     ]
-        [LShift A  R    S      T         G         M        N      E   I   O   Quote ]
-        [LCtrl  Z  X    C      D         V         K        H      ,   .   /   Bslash]
-        [n      n  LGui LAlt   Space     BSpace    Delete   Enter '{' '}'  n   n     ]
-        [n      n  t    Escape {NUM_PAD} n         {QWERTY} {AL_T} t   t   n   n     ]
+        [F1     F2 F3   F4     F5        F6        F7       F8      F9     F10   F11 F12   ]
+        ['`'    1  2    3      4         5         6        7       8      9     0   =     ]
+        [Tab    Q  W    F      P         B         J        L       U      Y     ;   -     ]
+        [LShift A  R    S      T         G         M        N       E      I     O   Quote ]
+        [LCtrl  Z  X    C      D         V         K        H       ,      .     /   Bslash]
+        [n      n  LGui LAlt   BSpace    Space     Enter    Delete '['    ']'    n   n     ]
+        [n      n  t    Escape {NUM_PAD} n         {QWERTY} RAlt    RShift RCtrl n   n     ]
     }
     { // (1) Qwerty
-        [F1     F2 F3   F4     F5        F6           F7     F8     F9  F10 F11 F12   ]
-        ['`'    1  2    3      4         5            6      7      8   9   0   =     ]
-        [Tab    Q  W    E      R         T            Y      U      I   O   P   -     ]
-        [LShift A  S    D      F         G            H      J      K   L   ;   Quote ]
-        [LCtrl  Z  X    C      V         B            N      M      ,   .   /   Bslash]
-        [n      n  LGui LAlt   Space     BSpace       Delete Enter '{' '}'  n   n     ]
-        [n      n  t    Escape {NUM_PAD} {COLEMAK}    n      {AL_T} t   t   n   n     ]
+        [F1     F2 F3   F4     F5        F6           F7    F8      F9     F10   F11 F12   ]
+        ['`'    1  2    3      4         5            6     7       8      9     0   =     ]
+        [Tab    Q  W    E      R         T            Y     U       I      O     P   -     ]
+        [LShift A  S    D      F         G            H     J       K      L     ;   Quote ]
+        [LCtrl  Z  X    C      V         B            N     M       ,      .     /   Bslash]
+        [n      n  LGui LAlt   BSpace    Space        Enter Delete '['    ']'    n   n     ]
+        [n      n  t    Escape {NUM_PAD} {COLEMAK}    n     RAlt    RShift RCtrl n   n     ]
     }
     { // (2) NumpadNav
         [t t t    t      t      t                     t t    t       t          t       t]
