c18322aa7baa07ce8c92d6f01292c67f3d755448
Paul Daniel Faria <Nashenas88@users.noreply.github.com>
2022-03-06 21:46:11 -0800

    Another layout update

--- src/layout.rs
+++ src/layout.rs
@@ -17,27 +17,27 @@ const QWERTY: Action = d(1);
 const LAYER: Action = l(2);
 const NUM_PAD: Action = d(3);
 const NAV: Action = d(4);
-const SYM: Action = d(5);
+const SYM: Action = l(5);
 const MMO: Action = d(6);
 
 pub(crate) static LAYERS: keyberon::layout::Layers<CustomAction> = keyberon::layout::layout! {
     { // (0) Colemak Dh
-        [F1     F2 F3    F4    F5     F6        F7     F8      F9     F10   F11 F12   ]
-        [Escape 1  2     3     4      5         6      7       8      9     0   =     ]
-        ['`'    Q  W     F     P      B         J      L       U      Y     ;   -     ]
-        [Tab    A  R     S     T      G         M      N       E      I     O   Quote ]
-        [LShift Z  X     C     D      V         K      H       ,      .     /   RShift]
-        [n      n  LCtrl LAlt  BSpace Space     Enter  Delete  RAlt   RCtrl n   n     ]
-        [n      n  t    {SYM}  LGui  {LAYER}   {LAYER} RGui   {SYM}   t     n   n     ]
+        [F1       F2 F3    F4    F5     F6        F7     F8      F9     F10   F11 F12   ]
+        [CapsLock 1  2     3     4      5         6      7       8      9     0   =     ]
+        [Escape   Q  W     F     P      B         J      L       U      Y     ;   -     ]
+        [Tab      A  R     S     T      G         M      N       E      I     O   Quote ]
+        [LShift   Z  X     C     D      V         K      H       ,      .     /   RShift]
+        [n        n  LCtrl LAlt  BSpace Space     Enter  Delete  RAlt   RCtrl n   n     ]
+        [n        n  t    {SYM}  LGui  {LAYER}   {LAYER} RGui   {SYM}   t     n   n     ]
     }
     { // (1) Qwerty
-        [F1     F2 F3    F4    F5     F6        F7      F8      F9     F10   F11 F12   ]
-        [Escape 1  2     3     4      5         6       7       8      9     0   =     ]
-        ['`'    Q  W     E     R      T         Y       U       I      O     P   -     ]
-        [Tab    A  S     D     F      G         H       J       K      L     ;   Quote ]
-        [LShift Z  X     C     V      B         N       M       ,      .     /   RShift]
-        [n      n  LCtrl LAlt  BSpace Space     Enter   Delete  RAlt   RCtrl n   n     ]
-        [n      n  t    {SYM}  LGui  {LAYER}   {LAYER}  RGui   {SYM}   t     n   n     ]
+        [F1       F2 F3    F4    F5     F6        F7      F8      F9     F10   F11 F12   ]
+        [CapsLock 1  2     3     4      5         6       7       8      9     0   =     ]
+        [Escape   Q  W     E     R      T         Y       U       I      O     P   -     ]
+        [Tab      A  S     D     F      G         H       J       K      L     ;   Quote ]
+        [LShift   Z  X     C     V      B         N       M       ,      .     /   RShift]
+        [n        n  LCtrl LAlt  BSpace Space     Enter   Delete  RAlt   RCtrl n   n     ]
+        [n        n  t    {SYM}  LGui  {LAYER}   {LAYER}  RGui   {SYM}   t     n   n     ]
     }
     { // (2) Left Layer Selector
         [ n        n     n      n         n        n        n  n         n         n      n     n      ]
@@ -66,14 +66,14 @@ pub(crate) static LAYERS: keyberon::layout::Layers<CustomAction> = keyberon::lay
         [n      n          LCtrl LAlt   BSpace Space    Enter   Delete RAlt RCtrl n n     ]
         [n      n          t    {SYM}   LGui  {LAYER}  {LAYER}  RGui  {SYM} t     n n     ]
     }
-    { // (5) Symbols
-        [{UF2}  n  n     n    n      n       n      n      n    n     n      n     ]
-        [n      1  2     3    4      5       6      7      8    9     0      n     ]
-        [n      !  @     #    $      %       ^      &     '('  ')'    *      n     ]
-        [n      = '_'    -    +      n       n      '"'   '{'  '}'    |      n     ]
-        [LShift n  n     <    >      n       n      Quote '['  ']'    Bslash RShift]
-        [n      n  LCtrl LAlt BSpace Space   Enter  Delete RAlt RCtrl n      n     ]
-        [n      n  t    {SYM} LGui  {LAYER} {LAYER} RGui  {SYM} t     n      n     ]
+    { // (5) Symbol
+        [{UF2}  n  n     n    n      n       n      n      n    n     n n     ]
+        [n      n  n     n    n      n       n      n      n    n     n n     ]
+        [n      1  2     3    4      5       6      7      8    9     0 n     ]
+        [n      = '_'    -    +      n       n     '('    '{'  '['    n n     ]
+        [LShift ~ '`'    |    Bslash n       n     ')'    '}'  ']'    n RShift]
+        [n      n  LCtrl LAlt BSpace Space   Enter  Delete RAlt RCtrl n n     ]
+        [n      n  t    {SYM} LGui  {LAYER} {LAYER} RGui  {SYM} t     n n     ]
     }
     { // (6) MMO
         [Escape F1 F2   F3     F4     F5       F6      F7    F8   F9         F10     n]
